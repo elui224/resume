ca7240fe01eb4c43b459e6334fc0efa345bf6be1
Eric Lui <elui224@gmail.com>
2020-05-27 16:07:15 -0400

    new styling, new layout

--- main.py
+++ main.py
@@ -7,27 +7,8 @@ with open('info.yml') as f:
     info = yaml.load(f, Loader=yaml.FullLoader)
 
 @app.route('/')
-def main():
-    return render_template('main.html', info=info)
-
-
-# @app.route("/")
-# def download_resume(path = None):
-#     if path is None:
-#         self.Error(400)
-#     try:
-#         return send_file(path, as_attachment=True)
-#     except Exception as e:
-#         self.log.exception(e)
-#         self.Error(400)
-
-# @app.route('/projects')
-# def projects():
-#     return render_template('projects.html', info=info)
-
-# @app.route('/about')
-# def about():
-#     return render_template('about.html', info=info)
+def index():
+    return render_template('index.html', info=info)
 
 
 if __name__ == '__main__':
